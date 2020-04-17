4d3f9fcc51995f688252d837aefea7298fb9859a
Zhangpeng <yaozhangpeng@CNyaozhangpeng.local>
2020-04-17 16:15:33 +0800

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module reptile
+
+go 1.14
+
+require (
+	github.com/canthefason/go-watcher v0.2.4 // indirect
+	github.com/fatih/color v1.9.0 // indirect
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"golang.org/x/net/html"
+)
+
+func main() {
+	urlArray := [1]string{"https://pokedex.org/1"}
+	for _, url := range urlArray {
+		fetch(url)
+	}
+}
+
+func fetch(url string) {
+	if !checkURL(url) {
+		fmt.Println("url is invalid",url)
+		os.Exit(1)
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR fetch request %s:%v\n", url, err)
+		os.Exit(1)
+	}
+	defer res.Body.Close()
+	defer io.Copy(ioutil.Discard, res.Body)
+
+	parseHTML(url, res.Body)
+}
+
+func checkURL(str string) bool {
+	return strings.HasPrefix(str, "https")
+}
+
+func parseHTML(url string, body io.Reader) {
+	doc, err := html.Parse(body)
+	if err != nil {
+		fmt.Errorf("parsing %s as HTML: %v\n ", url, err)
+	}
+	fmt.Println(doc)
+}
